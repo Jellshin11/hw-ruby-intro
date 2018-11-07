ac315baa8d59565a92dd5d22c6876d1ea8ddc16e
Jellshin11 <37632061+Jellshin11@users.noreply.github.com>
2018-11-06 20:21:26 -0600

    a Funcion

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -46,11 +46,11 @@ end
 # Part 2
 
 def hello(name)
-  "Hello,#{name}"
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[b-df-hj-np-tv-z]/i.match(s)!=nil
 end
 
 def binary_multiple_of_4? s
